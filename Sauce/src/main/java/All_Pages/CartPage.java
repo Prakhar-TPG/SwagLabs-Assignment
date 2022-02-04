07054eac8bad83d865685a97bafba3f6c3b6c5a2
Prakhar-TPG <95743694+Prakhar-TPG@users.noreply.github.com>
2022-02-04 12:02:03 +0530

    Update CartPage.java

--- Sauce/src/main/java/All_Pages/CartPage.java
+++ Sauce/src/main/java/All_Pages/CartPage.java
@@ -6,7 +6,7 @@ public class CartPage {
 
     public static final By CartProductName = By.xpath("//*[@id='item_1_title_link']/div");
     public static final By CartProductPrice = By.xpath("//div[@class= 'inventory_item_price']");
-	public static final By Item = By.xpath("//*[@id='item_1_title_link']/div");
+    public static final By Item = By.xpath("//*[@id='item_1_title_link']/div");
     public static final By Checkout = By.xpath("//button[@id = 'checkout']");
     public static String CartProdText;
     public static String CartPriceText;
