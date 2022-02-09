4358acfebfdfac71de44c8cccd95f8961acbbc1f
Prakhar-TPG <95743694+Prakhar-TPG@users.noreply.github.com>
2022-02-09 19:10:26 +0530

    Update ProductPage.java

--- Sauce/src/main/java/All_Pages/ProductPage.java
+++ Sauce/src/main/java/All_Pages/ProductPage.java
@@ -8,7 +8,7 @@ public class ProductPage {
     public static final By ProductPrice = By.xpath("//*[@id=\"inventory_container\"]/div/div[3]/div[2]/div[2]/div");
     public static final By add_cart = By.xpath("//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']");
     public static final By Cart_btn = By.xpath("//*[@id='shopping_cart_container']");
-    public static final By checkitem = By.xpath("//*[@id='shopping_cart_container']/a/span");
+//     public static final By checkitem = By.xpath("//*[@id='shopping_cart_container']/a/span");
     public static String ProdTextName;
     public static String PriceTextName;
 }
